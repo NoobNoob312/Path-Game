aab2a1205fdb41725e24248575a4709208214c86
NoobNoob312 <50655924+NoobNoob312@users.noreply.github.com>
2021-03-11 23:16:38 +0100

    Upload scripts

---
+++ Scripts/PlayerHealth.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+
+    [SerializeField] int health = 10;
+    [SerializeField] int healthDecrease = 1;
+    [SerializeField] Text healthText;
+    [SerializeField] AudioClip playerDamageSFX;
+    [SerializeField] Canvas gameOverCanvas;
+
+    public bool IsGameOver;
+
+    void Start()
+    {
+        healthText.text = health.ToString();
+        gameOverCanvas.enabled = false;
+        IsGameOver = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GetComponent<AudioSource>().PlayOneShot(playerDamageSFX);
+        HandleLose();
+    }
+
+    private void HandleLose()
+    {
+        if (health > 0)
+        {
+            health -= healthDecrease;
+            healthText.text = health.ToString();
+        }
+        else if (health <= 0)
+        {
+            IsGameOver = true;
+            FindObjectOfType<Waypoint>().enabled = false;
+            gameOverCanvas.enabled = true;
+            Time.timeScale = 0;
+        }
+    }
+
+}
---
+++ Scripts/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void ReloadGame()
+    {
+        FindObjectOfType<PlayerHealth>().IsGameOver = false;
+        SceneManager.LoadScene(0);
+        FindObjectOfType<Waypoint>().enabled = true;
+        Time.timeScale = 1;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
